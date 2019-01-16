a0ecb2979349e9b05339043b7b459cf6d1da79b3
nikigor <n.gorbadey@gmail.com>
2019-01-16 11:26:45 +0300

    Add idle hook example
    Signed-off-by: nikigor <n.gorbadey@gmail.com>

---
+++ rtos_idle/lib/os_routines.h
@@ -0,0 +1,7 @@
+#ifndef _OS_ROUTINES_
+#define _OS_ROUTINES_
+
+StaticTask_t xIdleTaskTCB;
+StackType_t uxIdleTaskStack[configMINIMAL_STACK_SIZE];
+
+#endif
---
+++ rtos_idle/main.c
@@ -0,0 +1,101 @@
+#include "stm32f0xx_ll_bus.h"
+#include "stm32f0xx_ll_gpio.h"
+#include "stm32f0xx_ll_rcc.h"
+#include "stm32f0xx_ll_system.h"
+
+#include "FreeRTOS.h"
+#include "task.h"
+#include "os_routines.h"
+
+/**
+  * System Clock Configuration
+  * The system Clock is configured as follow :
+  *    System Clock source            = PLL (HSI/2)
+  *    SYSCLK(Hz)                     = 48000000
+  *    HCLK(Hz)                       = 48000000
+  *    AHB Prescaler                  = 1
+  *    APB1 Prescaler                 = 1
+  *    HSI Frequency(Hz)              = 8000000
+  *    PLLMUL                         = 12
+  *    Flash Latency(WS)              = 1
+  */
+static void rcc_config(void)
+{
+        /* Set FLASH latency */
+        LL_FLASH_SetLatency(LL_FLASH_LATENCY_1);
+
+        /* Enable HSI and wait for activation*/
+        LL_RCC_HSI_Enable();
+        while (LL_RCC_HSI_IsReady() != 1);
+
+        /* Main PLL configuration and activation */
+        LL_RCC_PLL_ConfigDomain_SYS(LL_RCC_PLLSOURCE_HSI_DIV_2,
+                                    LL_RCC_PLL_MUL_12);
+
+        LL_RCC_PLL_Enable();
+        while (LL_RCC_PLL_IsReady() != 1);
+
+        /* Sysclk activation on the main PLL */
+        LL_RCC_SetAHBPrescaler(LL_RCC_SYSCLK_DIV_1);
+        LL_RCC_SetSysClkSource(LL_RCC_SYS_CLKSOURCE_PLL);
+        while (LL_RCC_GetSysClkSource() != LL_RCC_SYS_CLKSOURCE_STATUS_PLL);
+
+        /* Set APB1 prescaler */
+        LL_RCC_SetAPB1Prescaler(LL_RCC_APB1_DIV_1);
+
+        /* Set systick to 1ms */
+        SysTick_Config(48000000/1000);
+
+        /* Update CMSIS variable (which can be updated also
+         * through SystemCoreClockUpdate function) */
+        SystemCoreClock = 48000000;
+        return;
+}
+
+static void gpio_config(void)
+{
+        LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOC);
+        LL_GPIO_SetPinMode(GPIOC, LL_GPIO_PIN_9, LL_GPIO_MODE_OUTPUT);
+        LL_GPIO_SetPinMode(GPIOC, LL_GPIO_PIN_8, LL_GPIO_MODE_OUTPUT);
+        return;
+}
+
+void HardFault_Handler(void)
+{
+        LL_GPIO_SetOutputPin(GPIOC, LL_GPIO_PIN_9);
+        while (1);
+}
+
+void led_blink_green(void *p)
+{
+        (void) p;
+
+        while (1) {
+                LL_GPIO_TogglePin(GPIOC, LL_GPIO_PIN_8);
+                vTaskDelay(1000);
+        }
+        return;
+}
+
+
+StackType_t greenTaskStack[128];
+StaticTask_t greenTaskBuffer;
+
+void vApplicationIdleHook( void )
+{
+    LL_GPIO_SetOutputPin(GPIOC, LL_GPIO_PIN_9);
+}
+
+int main(void)
+{
+        rcc_config();
+        gpio_config();
+
+        xTaskCreateStatic(led_blink_green, "led_green", 128, NULL, 0,
+                          greenTaskStack, &greenTaskBuffer);
+
+        vTaskStartScheduler();
+
+        while (1);
+        return 0;
+}
