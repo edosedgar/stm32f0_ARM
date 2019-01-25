6d3d558b849450d19a4e9cc335bd8490725588b7
Edgar Kaziakhmedov <edos.edgar@gmail.com>
2019-01-25 15:32:41 +0300

    Add small fixes
    Signed-off-by: Edgar Kaziakhmedov <edos.edgar@gmail.com>

--- blank/main.c
+++
@@ -1,5 +0,0 @@
-int main(void)
-{
-    while (1);
-    return 0;
-}
---
+++ labs/01_blank/main.c
@@ -0,0 +1,11 @@
+/*
+ * It is just simple and pure template project
+ * It does absolutely nothing and indicates that toolchain
+ * is installed correctly.
+ */
+
+int main(void)
+{
+    while (1);
+    return 0;
+}
