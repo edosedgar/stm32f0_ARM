6623653bf98c7da8154c1ec62441592fa8a9054d
Edgar Kaziakhmedov <edos.edgar@gmail.com>
2019-03-15 01:05:36 +0300

    Add three more lectures
    Signed-off-by: Edgar Kaziakhmedov <edos.edgar@gmail.com>

---
+++ labs/08_rtc/main.c
@@ -0,0 +1,80 @@
+#include "stm32f0xx_ll_rcc.h"
+#include "stm32f0xx_ll_system.h"
+#include "stm32f0xx_ll_bus.h"
+#include "stm32f0xx_ll_gpio.h"
+
+/**
+  * System Clock Configuration
+  * The system Clock is configured as follow :
+  *    System Clock source            = PLL (HSI/2)
+  *    SYSCLK(Hz)                     = 48000000
+  *    HCLK(Hz)                       = 48000000
+  *    AHB Prescaler                  = 1
+  *    APB1 Prescaler                 = 1
+  *    HSI Frequency(Hz)              = 8000000
+  *    PLLMUL                         = 12
+  *    Flash Latency(WS)              = 1
+  */
+static void rcc_config()
+{
+    /* Set FLASH latency */
+    LL_FLASH_SetLatency(LL_FLASH_LATENCY_1);
+
+    /* Enable HSI and wait for activation*/
+    LL_RCC_HSI_Enable();
+    while (LL_RCC_HSI_IsReady() != 1);
+
+    /* Main PLL configuration and activation */
+    LL_RCC_PLL_ConfigDomain_SYS(LL_RCC_PLLSOURCE_HSI_DIV_2,
+                                LL_RCC_PLL_MUL_12);
+
+    LL_RCC_PLL_Enable();
+    while (LL_RCC_PLL_IsReady() != 1);
+
+    /* Sysclk activation on the main PLL */
+    LL_RCC_SetAHBPrescaler(LL_RCC_SYSCLK_DIV_1);
+    LL_RCC_SetSysClkSource(LL_RCC_SYS_CLKSOURCE_PLL);
+    while (LL_RCC_GetSysClkSource() != LL_RCC_SYS_CLKSOURCE_STATUS_PLL);
+
+    /* Set APB1 prescaler */
+    LL_RCC_SetAPB1Prescaler(LL_RCC_APB1_DIV_1);
+
+    /* Update CMSIS variable (which can be updated also
+     * through SystemCoreClockUpdate function) */
+    SystemCoreClock = 48000000;
+}
+
+/*
+ * Clock on GPIOC and set pin with Blue led connected
+ */
+static void gpio_config(void)
+{
+    LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOC);
+    LL_GPIO_SetPinMode(GPIOC, LL_GPIO_PIN_8, LL_GPIO_MODE_OUTPUT);
+    return;
+}
+
+static void rtc_config(void)
+{
+    LL
+    return;
+}
+
+/*
+ * Here we call configure all peripherals we need and
+ * start blinking upon current mode
+ */
+int main(void)
+{
+    rcc_config();
+    gpio_config();
+    rtc_config();
+
+    while (1) {
+        LL_GPIO_SetOutputPin(GPIOC, LL_GPIO_PIN_8);
+        delay();
+        LL_GPIO_ResetOutputPin(GPIOC, LL_GPIO_PIN_8);
+        delay();
+    }
+    return 0;
+}
